f89fd0017c7b2f420239c60dc48638e087348d8c
David Aminifard <47134024+davamini@users.noreply.github.com>
2019-12-03 11:13:29 -0800

    Update reddit_privacy_base.py

--- reddit_privacy_base.py
+++ reddit_privacy_base.py
@@ -4,10 +4,6 @@ import datetime
 import sqlite3
 import schedule
 
-reddit = praw.Reddit(client_id='epKPUwrZ5yb51g',
-                     client_secret="iwkv2-KQ3-VduKJDdca57vls35w", password='3De1OlR#',
-                     user_agent='ardonpage', username='ardonpage')
-
 
 subreddits = [
         
