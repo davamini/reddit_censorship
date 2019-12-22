54fa6afe5a8dd23e73290ac34b2e41db42259715
David Aminifard <47134024+davamini@users.noreply.github.com>
2019-12-22 11:39:02 -0800

    Update run_rc.py

--- run_rc.py
+++ run_rc.py
@@ -1,3 +1,7 @@
 from google_sheets_upload import scan_reddit_censorship
+import schedule
 
-scan_reddit_censorship()
+schedule.every().day.at("11:10").do(scan_reddit_censorship)
+
+while True:
+    schedule.run_pending()
